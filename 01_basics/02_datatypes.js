a25c9f8bacfa2bfb0d0357ed801009bdc1d46fe6
Kumar Ashutosh <kumarashutosh7917@gmail.com>
2024-01-16 09:29:43 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict" //treat all js code as newer version
+//alert (3+3) //we are using node not browser
+
+//number 
+//string
+//bigint
+//boolean
+//null=>standalone value
+//undefined
+//symbol
+
+console.log(typeof(null));//object
+console.log(typeof(undefined));//undefined
